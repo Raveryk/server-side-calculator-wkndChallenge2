7afb2ee4143b9edf0b977f1bfd01022197e2dba0
Reese Avery Kling <raveryk@gmail.com>
2021-04-16 19:51:51 -0500

    Set up clear button functionality and began implementing my POST method to send data to server.

--- Server/Public/client.js
+++ Server/Public/client.js
@@ -7,11 +7,38 @@ function onReady() {
     $('#minus-btn').on('click', subNums)
     $('#multiply-btn').on('click', multiplyNums)
     $('#divide-btn').on('click', divideNums);
+    $('#clear-btn').on('click', clearInputs)
 }
 
 
 function Calculation() {
+    // send Numbers to the server for calc
     console.log('You just calculated something!')
+
+    let newNums = {
+        num1: $('#num-one').val(),
+        num2: $('#num-two').val()
+    }
+
+    //ajax POST method to send data to the server for calculation
+    $.ajax({
+        method: 'POST',
+        url: '/numbers',
+        data: newNums
+    }) .then( function( response )  {
+        console.log('Adding Nums');
+
+        getNums();
+    }) .catch( function( error ) {
+        console.log('Error from the server', error);
+        alert('Sorry, we could not process your numbers');
+        
+    })
+}
+
+function clearInputs() {
+    $('#num-one').val('')
+    $('#num-two').val('')
 }
 
 function addNums() {
